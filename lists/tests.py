fbe0cb58600d279cbe417176029fc8d059fd9bae
rusekmac <mac.rusek1@gmail.com>
2017-08-08 14:02:36 +0100

    Base view returns HMTL

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -10,3 +12,10 @@ class HomePageTest(TestCase):
     def test_root_url_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
