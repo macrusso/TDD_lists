ff60e86b2926d3658ad3710ac8592fc68971b2d9
rusekmac <mac.rusek1@gmail.com>
2017-08-11 11:39:30 +0100

    Refactoring of functional test

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 
@@ -17,13 +18,15 @@ class HomePageTest(TestCase):
 
     def test_home_page_return_html(self):
         request = HttpRequest()
+        request.method = 'POST'
         response = home_page(request)
-
-        self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
             {'new_item_text': 'A new list item'}
         )
+
+        self.assertIn('A new list item', response.content.decode())
+
         self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_POST_request(self):
@@ -34,3 +37,20 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('A new list item', response.content.decode())
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The very first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The very first list item')
+        self.assertEqual(second_saved_item.text, 'Second item')
