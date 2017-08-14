7114d6b3b9486629165a3598fe165bf026c83997
rusekmac <mac.rusek1@gmail.com>
2017-08-14 16:01:43 +0100

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from time import sleep
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -37,10 +38,11 @@ class NewVisitorTest(LiveServerTestCase):
         # One types 'Learn python' into a text box
         input_box.send_keys('Learn Python')
 
-        # When one hits enter, the page updates and now the page lists
+        # When one hits enter, new page is open and now the page lists
         # '1: Learn Python' as an item on the list
         input_box.send_keys(Keys.ENTER)
-
+        user_list_url = self.browser.current_url
+        self.assertRegex(user_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Learn Python')
 
         # There is still a text box to enter another item
@@ -53,8 +55,49 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Learn Python')
         self.check_for_row_in_list_table('2: Use Python to make a web app')
 
-        # To remember the list, the page generates an unique URL for an user
-        self.fail('Finish the test!')
-        # Once revisited that URL, one realises its list is still there
+        # Another user opens the site
+        # New browser session to make sure that no information of previous user is coming through
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
 
-        # One closes the window
+        # New user visits the page and there is no data form previous user
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_elements_by_id('body')
+        self.assertNotIn('Learn Python', page_text)
+        self.assertNotIn('Use Python to make a web app', page_text)
+
+        # New user starts a new list by entering a new item
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
+
+        # New user gets its onw unique URL
+        new_user_list_url = self.browser.current_url
+        self.assertRegex(new_user_list_url, '/lists/.+')
+        self.assertNotEqual(new_user_list_url, user_list_url)
+
+        # Double check for previous user data
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Learn Python', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # New user closes page as well
+        self.fail('Finish the test!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,4 +20,5 @@ from lists import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list/$', views.view_list),
 ]
