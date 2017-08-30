3110366e7b63f16dde957d2df9b47e1445f8fd51
rusekmac <mac.rusek1@gmail.com>
2017-08-30 14:33:25 +0100

    using the existing list item form in the list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,7 +11,6 @@ class ItemValidationTest(FunctionalTest):
 
         # The page refreshes and there ia and error message saying
         # that list items cannot be blank
-
         self.browser.find_elements_by_css_selector('#id_text:valid')
         # error = self.browser.find_element_by_css_selector('.has-error')
         # self.assertEqual(error.text, "You can't have an empty list item")
@@ -38,6 +37,6 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Make tea\n')
 
         # User sees a helpful error message
-        self.check_for_row_in_list_table('2: Buy wellies')
+        self.check_for_row_in_list_table('2: Make tea')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You've already got this in your list")
