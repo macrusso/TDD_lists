080e88922b1c7965bd8c7db7ad2d8914eb3c3bfc
rusekmac <mac.rusek1@gmail.com>
2017-08-30 12:24:04 +0100

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -33,3 +33,11 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+        # User accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Make tea\n')
+
+        # User sees a helpful error message
+        self.check_for_row_in_list_table('2: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.models import Item, List
 
 
 class ItemFormTest(TestCase):
@@ -13,3 +14,11 @@ class ItemFormTest(TestCase):
         form = ItemForm()
         self.assertIn('placeholder="Enter an item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_save_handles_saving_ta_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
