7cdb05b95ce82212adf52643afccbedb42239109
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2017-10-21 00:07:59 +0300

    Add form for deleting todo items

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import redirect, render
-from lists.forms import ExistingListItemForm, ItemForm
-from lists.models import List
+from django.shortcuts import redirect, render, get_object_or_404
+from lists.forms import ExistingListItemForm, ItemForm, DeleteItemForm
+from lists.models import List, Item
 
 
 def home_page(request):
@@ -27,3 +27,13 @@ def view_list(request, list_id):
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
 
+def delete_item(request, list_id, item_id):
+    todo_list = get_object_or_404(List, pk=list_id)
+    item = get_object_or_404(Item, pk=item_id)
+
+    if request.method == 'POST':
+        form = DeleteItemForm(request.POST, instance=item)
+
+        if form.is_valid():
+            item.delete()
+            return redirect(todo_list)
